06c46aa099116bd352b7a4b31a6b446de3e77535
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-12 13:32:36 +0500

    add comment model and contoller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,14 @@ class MessagesController < ApplicationController
     @messages = Message.all.order('created_at DESC')
   end
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def show
 
   end
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-resources:messages
+  devise_for :users
+resources :messages do
+  resources :comments
+  end
 root 'messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
